9104663d952f9d1de1f9f9643e2fff5b3992dd2f
kombajno678 <38471959+kombajno678@users.noreply.github.com>
2021-05-06 19:39:04 +0200

    can start camunda process from ui

---
+++ src/sprint2/server.js
@@ -0,0 +1,112 @@
+const CAMUNDA_REST_URL = 'http://localhost:8080/engine-rest';
+const CAMUNDA_PROCESS_NAME = 'payment-retrival';
+
+const camundaRestResources = {
+    startProcess: CAMUNDA_REST_URL + '/process-definition/key/' + CAMUNDA_PROCESS_NAME + '/start',
+}
+
+// -------------------------------------------------
+// ------------ camunda service -------------
+// -------------------------------------------------
+const camundaResponses = [];
+
+const {
+    Client,
+    logger
+} = require('camunda-external-task-client-js');
+const open = require('open');
+
+const config = {
+    baseUrl: CAMUNDA_REST_URL,
+    use: logger,
+    asyncResponseTimeout: 10000
+};
+
+const client = new Client(config);
+
+client.subscribe('response-positive', async function ({
+    task,
+    taskService
+}) {
+    console.log(`To dobrze`);
+    camundaResponses.push({msg: "To dobrze", when: new Date().toISOString()});
+    await taskService.complete(task);
+});
+
+client.subscribe('response-negative', async function ({
+    task,
+    taskService
+}) {
+    console.log(`To źle`);
+    camundaResponses.push({msg: "To źle", when: new Date().toISOString()});
+    await taskService.complete(task);
+});
+
+
+
+
+
+
+// -------------------------------------------------
+
+
+
+
+
+
+const express = require('express');
+const app = express();
+const got = require('got');
+
+app.get('/hello', function (req, res) {
+    res.send('Hello World');
+})
+
+app.get('/messages', (req, res) => {
+    res.send({messages : camundaResponses});
+})
+app.post('/start', async (req, res) => {
+    console.log('/start');
+    let camundaStartProcessRequest = {
+        "variables": {
+            "amount": {
+                "value": 555,
+                "type": "long"
+            },
+            "item": {
+                "value": "item-xyz"
+            },
+            "response": {
+                "type" : "boolean",
+                "value": true
+            }
+        }
+    };
+
+    try {
+        const response = await got(camundaRestResources.startProcess, {
+            method: "POST",
+            json: camundaStartProcessRequest
+        });
+        console.log(response.body);
+        res.send(response.body);
+        //=> '<!doctype html> ...'
+    } catch (error) {
+        console.log(error.response.body);
+        res.send(error.response.body);
+        //=> 'Internal server error ...'
+    }
+
+    // now send post request with this body to camundaRestResources.startProcess
+
+
+})
+
+app.use(express.static('src/sprint2/static'));
+
+const server = app.listen(80, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
+})
