97b0dab932a2b853abfd4804f67e448ba371d681
Luvkitri <janjedrzejewski@gmail.com>
2021-05-12 21:37:57 +0200

    Payment functionallity + code format

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-const CAMUNDA_REST_URL = "http://localhost:8080/engine-rest";
+const CAMUNDA_REST_URL = 'http://localhost:8080/engine-rest';
 
 const camundaRestResources = {
-  message: CAMUNDA_REST_URL + "/message",
+  message: CAMUNDA_REST_URL + '/message',
 };
 
 // -------------------------------------------------
@@ -9,15 +9,15 @@ const camundaRestResources = {
 // -------------------------------------------------
 
 const camundaResponses: {
-  msg: string,
-  when: Date
+  msg: string;
+  when: Date;
 }[] = [];
 const warehouseOrders: {
-  id: string,
-  sent: boolean
+  id: string;
+  sent: boolean;
 }[] = [];
 
-import { Client, logger } from "camunda-external-task-client-js";
+import { Client, logger } from 'camunda-external-task-client-js';
 
 const config = {
   baseUrl: CAMUNDA_REST_URL,
@@ -28,16 +28,16 @@ const config = {
 const client = new Client(config);
 
 client.subscribe(
-  "validate-client-data",
+  'validate-client-data',
   async function ({ task, taskService }) {
-    const firstName: String = task.variables.get("firstName");
-    const lastName: String = task.variables.get("lastName");
+    const firstName: String = task.variables.get('firstName');
+    const lastName: String = task.variables.get('lastName');
 
     console.log(`Validating first name: "${firstName}"`);
     console.log(`Validating last name: "${lastName}"`);
 
     if (firstName.length < 3 || lastName.length < 3)
-      console.log("Error handling not implemented yet");
+      console.log('Error handling not implemented yet');
 
     await taskService.complete(task);
   }
@@ -51,24 +51,23 @@ client.subscribe(
     console.log('... and sending notification to user');
     camundaResponses.push({
       msg: `payment has been accepted for order "${task.processInstanceId}" `,
-      when: new Date(), 
-    })
+      when: new Date(),
+    });
     await taskService.complete(task);
-  })
+  }
+);
 
-client.subscribe(
-  'send-order-to-warehouse',
-  async ({ task, taskService }) => {
-    console.log('send-order-to-warehouse');
-    console.log('... brr sending order to warehouse, and updating database');
-    let order = {
-      id: task.processInstanceId + '',
-      sent: false,
-    }
-    warehouseOrders.push(order);
-    // updating databse, maybe later
-    await taskService.complete(task);
-  })
+client.subscribe('send-order-to-warehouse', async ({ task, taskService }) => {
+  console.log('send-order-to-warehouse');
+  console.log('... brr sending order to warehouse, and updating database');
+  let order = {
+    id: task.processInstanceId + '',
+    sent: false,
+  };
+  warehouseOrders.push(order);
+  // updating databse, maybe later
+  await taskService.complete(task);
+});
 
 client.subscribe(
   'update-order-state-after-shipment',
@@ -76,7 +75,9 @@ client.subscribe(
     console.log('update-order-state-after-shipment');
     console.log('... brr updating order state in database');
 
-    let order = warehouseOrders.find(order => order.id == task.processInstanceId);
+    let order = warehouseOrders.find(
+      (order) => order.id == task.processInstanceId
+    );
     if (order) {
       console.log('... warehouse order marked as sent');
       order.sent = true;
@@ -90,9 +91,21 @@ client.subscribe(
       when: new Date(),
     });
     await taskService.complete(task);
-  })
+  }
+);
 
-  
+client.subscribe(
+  'redirect-client-to-payment-provider',
+  async ({ task, taskService }) => {
+    const paymentMethod: String = task.variables.get('paymentMethod');
+
+    console.log('redirect-client-to-payment-provider');
+    console.log(`... brr redirecting client to payment provider. Chosen method: ${paymentMethod}`);
+
+    await taskService.complete(task);
+  }
+);
+
 // -------------------------------------------------
 // -------------------- express --------------------
 //  (for interaction between frontend and camunda)
@@ -100,21 +113,22 @@ client.subscribe(
 // -------------------------------------------------
 
 const PORT = 80;
-import express from "express";
+import express from 'express';
 import cors from 'cors';
+import { json } from 'body-parser';
 
 const app = express();
-const got = require("got");
+const got = require('got');
 
 app.use(cors());
-app.use(express.static("./static"));
+app.use(express.static('./static'));
 app.use(express.json());
 
-app.get("/hello", function (req: any, res: any) {
-  res.send("Hello World");
+app.get('/hello', function (req: any, res: any) {
+  res.send('Hello World');
 });
 
-app.get("/messages", (req: any, res: any) => {
+app.get('/messages', (req: any, res: any) => {
   res.send({ messages: camundaResponses });
 });
 
@@ -124,9 +138,9 @@ app.get('/warehouse/orders', (req: any, res: any) => {
 
 app.post('/warehouse/realiseorder', async (req: any, res: any) => {
   console.log('/warehouse/realiseorder');
-  
+
   const payload = {
-    messageName: "orderSentConfirmation",
+    messageName: 'orderSentConfirmation',
     processInstanceId: req.body.id,
     resultEnabled: true,
   };
@@ -134,9 +148,9 @@ app.post('/warehouse/realiseorder', async (req: any, res: any) => {
 
   try {
     const response = await got(camundaRestResources.message, {
-      method: "POST",
+      method: 'POST',
       json: payload,
-      responseType: "json",
+      responseType: 'json',
     });
     console.log(`orderSentConfirmation success`);
     res.send({ id: req.body.id });
@@ -147,17 +161,17 @@ app.post('/warehouse/realiseorder', async (req: any, res: any) => {
   }
 });
 
-app.get("/confirmCartContents", async (req: any, res: any) => {
+app.get('/confirmCartContents', async (req: any, res: any) => {
   const payload = {
-    messageName: "newOrderMessage",
+    messageName: 'newOrderMessage',
     resultEnabled: true,
   };
 
   try {
     const response = await got(camundaRestResources.message, {
-      method: "POST",
+      method: 'POST',
       json: payload,
-      responseType: "json",
+      responseType: 'json',
     });
     let newProcessId = response.body[0].processInstance.id;
     console.log(`new process started, id = ${newProcessId}`);
@@ -168,22 +182,53 @@ app.get("/confirmCartContents", async (req: any, res: any) => {
   }
 });
 
-app.post("/submitClientData", async (req: any, res: any) => {
+app.post('/payment', async (req: any, res: any) => {
   const payload = {
-    messageName: "clientDataMessage",
+    messageName: 'clientDataMessage',
     processInstanceId: req.body.id,
     processVariables: {
-      firstName: { value: req.body.firstName, type: "String" },
-      lastName: { value: req.body.lastName, type: "String" },
+      paymentMethod: { value: req.body.paymentMethod, type: 'String' },
+    },
+    resultEnabled: true,
+  };
+
+  try {
+    await got(camundaRestResources.message, {
+      method: 'POST',
+      json: payload,
+      responseType: 'json',
+    });
+
+    console.log('Payment method submited');
+    res.status(200);
+  } catch (error) {
+    console.error('failed to submit payment method');
+    res.status(500);
+  }
+});
+
+app.post('/payment/status', (req: any, res: any) => {
+  let message = req.body.msg;
+  console.log(`Payment provider message: ${message}`);
+  res.status(200);
+});
+
+app.post('/submitClientData', async (req: any, res: any) => {
+  const payload = {
+    messageName: 'clientDataMessage',
+    processInstanceId: req.body.id,
+    processVariables: {
+      firstName: { value: req.body.firstName, type: 'String' },
+      lastName: { value: req.body.lastName, type: 'String' },
     },
     resultEnabled: true,
   };
 
   try {
     const response = await got(camundaRestResources.message, {
-      method: "POST",
+      method: 'POST',
       json: payload,
-      responseType: "json",
+      responseType: 'json',
     });
     console.log('client data submitted');
     res.send({});
