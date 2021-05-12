0b9735054e548f3a38392dcac6d059d94afcac92
Luvkitri <janjedrzejewski@gmail.com>
2021-05-12 20:48:05 +0200

    Added payment select functionallity + code format

--- static/index.js
+++ static/index.js
@@ -1,96 +1,129 @@
-window.addEventListener("load", () => {
-  console.log("loaded");
+window.addEventListener('load', () => {
+  console.log('loaded');
 
   setInterval(getMessages, 500);
-  getWarehouseOrders()
+  getWarehouseOrders();
 });
 
-const shopUrl = "http://localhost:80/";
+const shopUrl = 'http://localhost:80/';
 
 const shopResources = {
-  confirmCartContents: shopUrl + "confirmCartContents",
-  submitClientData: shopUrl + "submitClientData",
-  messages: shopUrl + "messages",
+  confirmCartContents: shopUrl + 'confirmCartContents',
+  submitClientData: shopUrl + 'submitClientData',
+  messages: shopUrl + 'messages',
+  payment: shopUrl + 'payment',
 
-  warehouseOrders: shopUrl + "warehouse/orders",
-  warehouseRealiseOrder: shopUrl + "warehouse/realiseorder",
+  warehouseOrders: shopUrl + 'warehouse/orders',
+  warehouseRealiseOrder: shopUrl + 'warehouse/realiseorder',
 };
 
 function onConfirmCartContents() {
-  console.log("onConfirmCartContents");
+  console.log('onConfirmCartContents');
 
   fetch(shopResources.confirmCartContents)
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem("id", data.id);
+      localStorage.setItem('id', data.id);
       //document.getElementById('current-process-id').innerHTML = data.id;
-      console.log("Success");
+      console.log('Success');
     });
 }
 
 function onSubmitClientData(e) {
-  console.log("onSubmitClientData");
+  console.log('onSubmitClientData');
   e.preventDefault();
 
   const formData = new FormData(e.target);
 
   const payload = {
-    id: localStorage.getItem("id"),
-    firstName: formData.get("firstName"),
-    lastName: formData.get("lastName"),
+    id: localStorage.getItem('id'),
+    firstName: formData.get('firstName'),
+    lastName: formData.get('lastName'),
   };
 
   fetch(shopResources.submitClientData, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(payload),
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json',
     },
   }).then(() => {
-    console.log("Success");
-    localStorage.removeItem("id");
+    console.log('Success');
+    localStorage.removeItem('id');
   });
+}
+
+function submitPaymentMethod() {
+  let selectedValue = document.getElementById('paymentSelect').value;
+
+  console.log(selectedValue);
+
+  if (selectedValue === '') {
+    return;
+  }
+
+  const payload = {
+    id: localStorage.getItem('id'),
+    paymentMethod: selectedValue,
+  };
+
+  fetch(shopResources.payment, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(() => {
+      console.log('submitPaymentMethod > Success');
+    })
+    .catch((error) => {
+      console.warn('submitPaymentMethod > Error');
+      console.warn(error);
+    });
 }
 
 function getMessages() {
   fetch(shopResources.messages)
     .then((response) => response.json())
     .then((json) => {
-      let messages = document.getElementById("messages");
-      messages.innerHTML = "";
+      let messages = document.getElementById('messages');
+      messages.innerHTML = '';
       json.messages.forEach((m) => {
-        let newMessage = document.createElement("div");
+        let newMessage = document.createElement('div');
         newMessage.innerHTML = `${new Date(m.when).toISOString()} : "${m.msg}"`;
         messages.appendChild(newMessage);
       });
-    }).catch(error => {
+    })
+    .catch((error) => {
       console.warn(error);
     });
 }
 
 function setOrderAsRealised(orderId) {
-
   console.log(`setOrderAsRealised > will set order ${orderId} as realised`);
 
   let payload = {
-    id: orderId
+    id: orderId,
   };
 
   fetch(shopResources.warehouseRealiseOrder, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(payload),
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json',
     },
-  }).then(() => {
-    console.log("setOrderAsRealised > Success");
-  }).catch(error => {
-    console.warn("setOrderAsRealised > Error");
-    console.warn(error);
-  }).finally(() => {
-    getWarehouseOrders();
-  });
-
+  })
+    .then(() => {
+      console.log('setOrderAsRealised > Success');
+    })
+    .catch((error) => {
+      console.warn('setOrderAsRealised > Error');
+      console.warn(error);
+    })
+    .finally(() => {
+      getWarehouseOrders();
+    });
 }
 
 function getWarehouseOrders() {
@@ -102,19 +135,18 @@ function getWarehouseOrders() {
       if (json.orders.length == 0) {
         ordersContainer.innerHTML = '(no orders)';
       } else {
-        json.orders.forEach(order => {
-          let orderElement = document.createElement("div");
+        json.orders.forEach((order) => {
+          let orderElement = document.createElement('div');
           let html = '';
           html = `id:${order.id}, sent?:${order.sent}`;
           if (!order.sent) {
-            html = html + `<button class="btn" onClick="setOrderAsRealised('${order.id}')">mark as sent</button><br>`;
+            html =
+              html +
+              `<button class="btn" onClick="setOrderAsRealised('${order.id}')">mark as sent</button><br>`;
           }
           orderElement.innerHTML = html;
           ordersContainer.appendChild(orderElement);
         });
       }
-
     });
-
-
 }
