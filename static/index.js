8921de1b3383e2d67e50ba6d614fbaf5d502957d
kombajno678 <kombajno678@gmail.com>
2021-05-13 00:41:50 +0200

    better ui

--- static/index.js
+++ static/index.js
@@ -1,9 +1,5 @@
-window.addEventListener('load', () => {
-  console.log('loaded');
-
-  //setInterval(getMessages, 500);
-  getWarehouseOrders();
-});
+const STEP_STORAGE_KEY = 'shop-step';
+const ORDER_STORAGE_KEY = 'shop-id';
 
 const shopUrl = 'http://localhost:80/';
 
@@ -18,27 +14,141 @@ const shopResources = {
   warehouseRealiseOrder: shopUrl + 'warehouse/realiseorder',
 };
 
+
+var currentStep = 1;
+const elementsInsideSteps = [{
+    step: 1,
+    elements: ['button-confirmcart'],
+    containerId: 'client-step1'
+  },
+  {
+    step: 2,
+    elements: ['form-contact'],
+    containerId: 'client-step2'
+  },
+  {
+    step: 3,
+    elements: ['paymentSelect', 'button-payment'],
+    containerId: 'client-step3'
+  },
+  {
+    step: 4,
+    elements: ['button-pay'],
+    containerId: 'client-step4'
+  },
+  {
+    step: 5,
+    elements: ['button-getmessages'],
+    containerId: 'client-step5'
+  },
+]
+
+
+window.addEventListener('load', () => {
+  console.log('loaded');
+
+  //setInterval(getMessages, 500);
+  getWarehouseOrders();
+
+  if (localStorage.getItem(STEP_STORAGE_KEY)) {
+    setCurrentStep(localStorage.getItem(STEP_STORAGE_KEY));
+  } else {
+    setCurrentStep(1);
+  }
+  updateOrderIdUi()
+});
+
+function updateOrderIdUi() {
+  document.getElementById('order-id').innerHTML = localStorage.getItem(ORDER_STORAGE_KEY);
+
+}
+
+function resetState() {
+  localStorage.clear(ORDER_STORAGE_KEY);
+  localStorage.clear(STEP_STORAGE_KEY);
+  setCurrentStep(1);
+  updateOrderIdUi();
+
+}
+
+function advanceStep() {
+
+  if (currentStep >= 5) {
+    return;
+  }
+
+  currentStep += 1;
+  setCurrentStep(currentStep);
+
+}
+
+function setCurrentStep(step) {
+
+  console.log('setCurrentStep, step = ', step);
+
+  elementsInsideSteps.filter(e => e.step != step).forEach(s => {
+    s.elements.forEach(element => element.disabled = true);
+    document.getElementById(s.containerId).setAttribute('class', 'step-disabled');
+
+  });
+
+  let x = elementsInsideSteps.find(e => e.step == step)
+  x.elements.forEach(element => {
+    element.disabled = false;
+  });
+  document.getElementById(x.containerId).setAttribute('class', 'step-active');
+
+  localStorage.setItem(STEP_STORAGE_KEY, currentStep);
+
+  updateOrderIdUi();
+
+}
+
+function setStep(step, enable) {
+  if (enable === true || enable === false) {
+    let stepElement = elementsInsideSteps.find(s => s.step == step)
+    stepElement.elements.forEach(element => {
+      element.disabled = !enable;
+    });
+    document.getElementById(stepElement.containerId).setAttribute('class', enable ? 'step-active' : 'step-disabled');
+
+
+  }
+
+}
+
+
+
+
 function onConfirmCartContents() {
+  setStep(1, false);
   console.log('onConfirmCartContents');
 
   fetch(shopResources.confirmCartContents)
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem('id', data.id);
+      localStorage.setItem(ORDER_STORAGE_KEY, data.id);
       //document.getElementById('current-process-id').innerHTML = data.id;
       console.log('Success');
-      console.log(localStorage.getItem('id'));
+      console.log(localStorage.getItem(ORDER_STORAGE_KEY));
+
+      advanceStep();
+
+    }).catch((error) => {
+      console.error(error);
+      setStep(1, true);
     });
 }
 
 function onSubmitClientData(e) {
+  setStep(2, false);
   console.log('onSubmitClientData');
   e.preventDefault();
 
   const formData = new FormData(e.target);
 
   const payload = {
-    id: localStorage.getItem('id'),
+    id: localStorage.getItem(ORDER_STORAGE_KEY),
     firstName: formData.get('firstName'),
     lastName: formData.get('lastName'),
   };
@@ -51,11 +161,17 @@ function onSubmitClientData(e) {
     },
   }).then(() => {
     console.log('Success');
-    //localStorage.removeItem('id');
+
+    advanceStep();
+
+  }).catch((error) => {
+    console.error(error);
+    setStep(2, true);
   });
 }
 
 function submitPaymentMethod() {
+  setStep(3, false);
   let selectedValue = document.getElementById('paymentSelect').value;
 
   console.log(selectedValue);
@@ -65,57 +181,71 @@ function submitPaymentMethod() {
   }
 
   const payload = {
-    id: localStorage.getItem('id'),
+    id: localStorage.getItem(ORDER_STORAGE_KEY),
     paymentMethod: selectedValue,
   };
   console.log('payload');
   console.log(payload);
 
   fetch(shopResources.payment, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then(() => {
-      console.log('submitPaymentMethod > Success');
-    })
-    .catch((error) => {
-      console.warn('submitPaymentMethod > Error');
-      console.warn(error);
-    });
-}
-
-function submitPayment() {
-  setTimeout(() => {
-    const payload = {
-      id: localStorage.getItem('id'),
-      msg: 'Payment succeded!',
-    };
-
-    fetch(shopResources.paymentProvider, {
       method: 'POST',
-      header: {
+      headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(payload),
     })
+    .then(() => {
+      console.log('submitPaymentMethod > Success');
+
+      advanceStep();
+
+    })
+    .catch((error) => {
+      console.warn('submitPaymentMethod > Error');
+      console.warn(error);
+      setStep(3, true);
+    });
+}
+
+function submitPayment() {
+
+  setStep(4, false);
+
+  document.getElementById('payment-state').innerHTML = 'Processing payment ...';
+
+  setTimeout(() => {
+    const payload = {
+      id: localStorage.getItem(ORDER_STORAGE_KEY),
+      msg: 'Payment succeded!',
+    };
+
+    fetch(shopResources.paymentProvider, {
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      })
       .then(() => {
         console.log('submitPayment > Success');
+        document.getElementById('payment-state').innerHTML = 'Payment successful';
+        advanceStep();
+
       })
       .catch((error) => {
         console.warn('submitPayment > Error');
         console.warn(error);
+        setStep(4, true);
+        document.getElementById('payment-state').innerHTML = 'Error processing payment';
       });
   }, 5000);
 }
 
-function getMessages() {
+function getMessages(destId) {
   fetch(shopResources.messages)
     .then((response) => response.json())
     .then((json) => {
-      let messages = document.getElementById('messages');
+      let messages = document.getElementById(destId);
       messages.innerHTML = '';
       json.messages.forEach((m) => {
         let newMessage = document.createElement('div');
@@ -136,12 +266,12 @@ function setOrderAsRealised(orderId) {
   };
 
   fetch(shopResources.warehouseRealiseOrder, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
     .then(() => {
       console.log('setOrderAsRealised > Success');
     })
